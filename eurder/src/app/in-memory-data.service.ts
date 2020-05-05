f67d57222cf67bb92729ba4b3839408f91fd944c
MatiasVL <matias.vanlangenhove@gmail.com>
2020-05-05 15:57:07 +0300

    started on create item

---
+++ eurder/src/app/in-memory-data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import {Item} from "./item";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const items = [
+      {id: '1', name: 'ball', description: 'A nice ball', price: 2, stockUrgency: 'green', stock: 10},
+      {id: '2', name: 'book', description: 'A fun book', price: 10, stockUrgency: 'green', stock: 150},
+      {id: '3', name: 'shoes', description: 'Great shoes', price: 60, stockUrgency: 'orange', stock: 100},
+      {id: '4', name: 'jacket', description: 'A cool jacket', price: 80, stockUrgency: 'green', stock: 110},
+      {id: '5', name: 'table', description: 'A sturdy table', price: 250, stockUrgency: 'red', stock: 140},
+      {id: '6', name: 'food', description: 'Delicious food', price: 50, stockUrgency: 'green', stock: 150},
+      {id: '7', name: 'drinks', description: 'Refreshing drinks', price: 4, stockUrgency: 'orange', stock: 160},
+      {id: '8', name: 'bread', description: 'Tasty bread', price: 2, stockUrgency: 'red', stock: 80}
+    ];
+    return {items};
+  }
+}
