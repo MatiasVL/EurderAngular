74f137e9f46dd6b049c7c5cba6d2644962849238
MatiasVL <matias.vanlangenhove@gmail.com>
2020-05-06 23:20:47 +0300

    tried adding create item, does not work

---
+++ eurder/src/app/item-update/item-update.component.ts
@@ -0,0 +1,41 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Item} from "../item";
+import {ActivatedRoute} from "@angular/router";
+import {ItemService} from "../item.service";
+import {Location} from "@angular/common";
+
+@Component({
+  selector: 'app-item-update',
+  templateUrl: './item-update.component.html',
+  styleUrls: ['./item-update.component.css']
+})
+
+export class ItemUpdateComponent implements OnInit {
+  @Input() item: Item;
+
+  constructor(
+    private route: ActivatedRoute,
+    private itemService: ItemService,
+    private location: Location
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getItem();
+  }
+
+  getItem(): void {
+    const id=this.route.snapshot.paramMap.get('id');
+    this.itemService.getItem(id).subscribe(item=>this.item=item);
+  }
+
+  goBack():void {
+    this.location.back();
+  }
+
+  update():void {
+    this.itemService.updateItem(this.item)
+      .subscribe(()=>this.goBack());
+  }
+
+}
